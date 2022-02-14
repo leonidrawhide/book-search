43766e442f30d4e1aa743d7332061804bf1997fe
Leonid (notebook) <leonidrawhide@gmail.com>
2022-02-14 08:48:57 +0300

    added load more functionality and redux (not used atm)

--- src/components/BookList/BookList.js
+++ src/components/BookList/BookList.js
@@ -5,28 +5,45 @@ export default class BookList extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			startIndex: 15,
+			startIndex: 0,
 			books: []
 		};
 	}
 
 	componentDidMount() {
+		this.loadBooks()
+	}
+
+	componentDidUpdate(prevProps, prevState) {
+		if (prevState.startIndex !== this.state.startIndex) {
+		  this.loadBooks();
+		}
+	}
+
+	loadBooks = () => {
 		fetch("https://www.googleapis.com/books/v1/volumes?q=search+terms&maxResults=30&startIndex=" + this.state.startIndex)
 			.then((response) => response.json())
 			.then((response) => {				
 				// console.log(response)
-				this.setState({
-					books: response.items
-				})
+				this.setState((prevState) => ({
+					books: [...prevState.books, ...response.items]
+				}))
 				
 				// console.log(this.state.books[0].volumeInfo.title)
 				
 			})
 	}
 
+	loadMore = () => {
+		this.setState((prevState) => ({
+			startIndex: prevState.startIndex + 30
+		}));
+	}
+	
+
 	render() {
 		const {books} = this.state;
-		console.log(books)
+		console.log(this.state.startIndex)
 		if (books.length == 0) {
 			return <div>Loading</div>
 		} else {
@@ -34,7 +51,10 @@ export default class BookList extends Component {
 				<div className='book-list'>
 					{books.map((item) => (
 						<BookListCard book={item} />	
-					))}				
+					))}	
+					<div className='book-list__load-more'>
+						<button onClick={this.loadMore}>Load more</button>	
+					</div>			
 			</div>
 			)
 		}
