7efbddc060cf5c02e153a1189e0c669408a204ac
Leonid (notebook) <leonidrawhide@gmail.com>
2022-02-17 17:17:24 +0300

    added more info to cards on main page

--- src/components/BookListCard/BookListCard.js
+++ src/components/BookListCard/BookListCard.js
@@ -1,14 +1,20 @@
 import React from 'react'
 
 export default function BookListCard(props) {
-	const imgLink = props.book.volumeInfo?.imageLinks?.thumbnail
-	const title =  props.book.volumeInfo?.title
+	const imgLink = props.book.volumeInfo?.imageLinks?.thumbnail;
+	const title =  props.book.volumeInfo?.title;
+	const author = props.book.volumeInfo?.authors?.[0];
+	console.log(props.book.volumeInfo?.authors?.[0])
+	const category = props.book.volumeInfo?.categories?.[0];
+
   	return (
 	<div className='card'>
 		<div className='card__cover'>
 			<img src={imgLink} alt="обложка" />
 		</div>
+		<h5 className='card__category'>{category}</h5>
 		<h3 className='card__title'>{title}</h3>
+		<h5 className='card__author'>{author}</h5>
 	</div>
   )
 }
